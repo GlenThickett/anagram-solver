f4ef88b5506550c61c0d638e4105aea6f38ebbae
GlenThickett <37292501+GlenThickett@users.noreply.github.com>
2025-12-15 11:11:19 +1100

    Update app.py

--- app.py
+++ app.py
@@ -108,9 +108,9 @@ build_anagram_map()
 def home():
     return render_template("index.html")
 
-@app.route("/how-to-solve")
+@app.route("/how-to-solve-anagrams")
 def how_to_solve():
-    return render_template("how_to_solve.html")
+    return render_template("how_to_solve_anagrams.html")
 
 @app.route("/solve")
 def solve():
@@ -137,4 +137,5 @@ def solve():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
